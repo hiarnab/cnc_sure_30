c0a9d99e1740f9b90479560c90e188cd9659a51b
soumya19991 <74457972+soumya19991@users.noreply.github.com>
2025-06-02 21:58:24 +0530

    jj

---
+++ resources/views/layout/frontend/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>sure30</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+
+</head>
+
+<body>
+    @include('layout.frontend.partials.header')
+    <main>
+        @yield('content')
+    </main>
+
+</body>
+
+</html>
