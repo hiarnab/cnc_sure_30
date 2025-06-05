f3743de5d98b718fdd41d56f24aebf93ce29ef62
hiarnab <hiamarnab@gmail.com>
2025-06-05 11:26:26 +0530

    sdfg

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
-
+use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+use App\Http\Controllers\admin\DashboardController;
+
 Route::get('/', function () {
     return view('frontend.pages.home');
 })->name('home');
 
+Route::get('/dashboard',[DashboardController::class, 'index']);
