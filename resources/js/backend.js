ec6d27358e16fa9d99cd7f3780fdf9229ed7a35d
It's Satyajit💫 <77627641+Its-Satyajit@users.noreply.github.com>
2025-06-06 20:49:03 +0530

    feat: Add Dwolla, GL, and Monero Dark SVG assets
    The changes in this commit add three new SVG assets to the `public/build/assets` directory:
    
    1. `dwolla-CF6Ywlpz.svg`: This is the Dwolla logo SVG.
    2. `gl-D3PIupNB.svg`: This is the GL (Greenland) flag SVG.
    3. `monero-dark-D1VwBLum.svg`: This is the Monero (dark) logo SVG.
    
    These assets are likely used in the application's user interface or branding, and their addition will enhance the visual experience for users.

--- resources/js/backend.js
+++ resources/js/backend.js
@@ -1,1 +1,7 @@
 import "./bootstrap";
+import "./lib/demo-theme.min";
+import "./lib/demo.min";
+import "./lib/tabler.esm.min";
+import "./lib/tabler.min";
+import "../libs/apexcharts/dist/apexcharts.esm";
+import "../libs/countup.js/dist/countUp.min";
